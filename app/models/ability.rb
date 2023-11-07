642efe7b9d62caa7d35530ed8a438e6e9ec49a27
rakad-dwairi <rakadissam@gmail.com>
2023-11-07 22:19:57 +0300

    Modified Routes Files

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,22 +5,27 @@ class Ability
 
   def initialize(user)
     if user.role == 'jobseeker'
-        can :read, JobPost  # Job Seekers can read job posts
-        can :create, JobApplication  # Job Seekers can apply to jobs
-        can :update, JobApplication, :user_id => user.id  # Job Seekers can update their applications
-        can :read, JobApplication, :user_id => user.id  # Job Seekers can view their applications
-        can :read, User, :user_id => user.id # Job Seekers can view their User Profile
-        can :update, User, :user_id => user.id # Job Seekers can update their User Profile
-        cannot :read, User
+      jobseeker_abilities(user)
+    elsif user.role == 'admin'
+      admin_abilities
     end
+  end
 
-    if user.role == 'admin'
-        can :manage, JobPost  # Admin Users can manage job posts
-        can :read, JobApplication  # Admin Users can view job applications
-        can :update, JobApplication  # Admin Users can update job applications
-        can :manage, User # Admin Users can manage job applications
-        cannot :destroy, [User, JobApplication]  # Admin Users cannot destroy users or job applications
-    end
+  private
 
+  def jobseeker_abilities(user)
+    can :read, JobPost
+    can :create, JobApplication
+    can [:read, :update], JobApplication, user_id: user.id
+    can [:read, :update], User, id: user.id
+    cannot :read, User
+  end
+
+  def admin_abilities
+    can :manage, JobPost
+    can :read, JobApplication
+    can :update, JobApplication
+    can :manage, User
+    cannot :destroy, [User, JobApplication]
   end
 end
--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,9 +1,7 @@
 class JobPost < ApplicationRecord
     belongs_to :user
     before_validation :set_default_date, on: :create
-  
-    # Other model code
-  
+    
     private
   
     def set_default_date
